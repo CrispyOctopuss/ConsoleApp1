c04e0092de8c08d6e35d5dc36ea2814f4dd3e9fe
CrispyOctopuss <daiokaze@gmail.com>
2020-08-16 20:37:29 +0400

    напоправлял либу

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Class1 test = new Class1("test");
+            Class1 test = new Class1("test", "Тестерунг");
             Console.WriteLine("Hello World!");
         }
     }
